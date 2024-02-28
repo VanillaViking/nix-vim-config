c663e29ed034f52b82c164abe6a75afd67d3f7b0
Vanilla Viking <ashwinr2k2@gmail.com>
2024-02-29 08:40:16 +1100

    commands fix

--- config/usercommands.nix
+++ config/usercommands.nix
@@ -1,6 +1,8 @@
 {
-  userCommands = [
-    B.command = "G checkout -b ashwin/<q-args>";
-    B.nargs = "1";
-  ];
+  userCommands = {
+    B = {
+      command = "G checkout -b ashwin/<q-args>";
+      nargs = "1";
+    }
+  };
 }
